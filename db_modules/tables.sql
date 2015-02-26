0f4158c96342b95b701c4df418952ef41502c46b
sedenardi <sedenardi@gmail.com>
2015-02-26 20:41:09 +0000

    remove charset from db schema

--- db_modules/tables.sql
+++ db_modules/tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `Actors` (
   `Birthday` date DEFAULT NULL,
   `RecordedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`ActorID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Appearances` (
   `AppearanceID` int(11) NOT NULL AUTO_INCREMENT,
@@ -16,35 +16,34 @@ CREATE TABLE IF NOT EXISTS `Appearances` (
   PRIMARY KEY (`AppearanceID`),
   KEY `idx_Appearances_Actor` (`ActorID`),
   KEY `idx_Appearances_Title` (`TitleID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `BaseTitles` (
   `BaseTitleID` varchar(10) NOT NULL,
   `DisplayName` varchar(20) NOT NULL,
   `Sort` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `CommonTitleActors` (
   `ParentTitleID` varchar(10) DEFAULT NULL,
   `TitleID` varchar(10) DEFAULT NULL,
   `ActorID` varchar(10) DEFAULT NULL,
   KEY `IX_CommonTitleActors` (`ParentTitleID`,`TitleID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `ParentTitleActors` (
   `ParentTitleID` varchar(10) DEFAULT NULL,
   `ActorID` varchar(10) DEFAULT NULL,
   KEY `IX_ParentTitleActors` (`ActorID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `ProcessActors` (
   `ActorID` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `ProcessTitles` (
   `TitleID` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Titles` (
   `TitleID` varchar(10) NOT NULL,
@@ -58,5 +57,4 @@ CREATE TABLE IF NOT EXISTS `Titles` (
   `RecordedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`TitleID`),
   KEY `idx_Titles_ParentTitleID_AirDate` (`ParentTitleID`,`AirDate`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB;
